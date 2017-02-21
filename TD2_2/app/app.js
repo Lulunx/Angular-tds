0c77dec267d6e066bfd4c99f6e77b6ea1a9953b5
Lulunx <lucasgancel@gmail.com>
2017-02-21 16:27:51 +0100

    Fin du TD
    TD fini avec filtres, ajouts, modifications fonctionnel !

--- TD2_2/app/app.js
+++ TD2_2/app/app.js
@@ -23,27 +23,46 @@ ContactApp.controller("ContactController",['filterFilter', function FilterContro
 
 
     this.contact=[];
-    this.tmpContact=[];
+    this.tmpcontact={
+        "firstname": "",
+        "lastname": "",
+        "mail": ""
+    };
     this.supression=0;
     this.operation="Ajouter";
     this.visible=0;
 
-    this.toAdd=function(contact){
+    this.toAdd=function(){
       self.visible=1;
       self.operation="Ajouter";
+      self.tmpcontact.firstname="";
+      self.tmpcontact.lastname="";
+      self.tmpcontact.mail="";
     };
 
-    this.toUpdate=function(){
+    this.toUpdate=function(contact){
+        self.contact=contact;
         self.visible=1;
         self.operation="Modifier";
+        self.tmpcontact.firstname=contact.firstname;
+        self.tmpcontact.lastname=contact.lastname;
+        self.tmpcontact.mail=contact.mail;
     };
 
     this.add=function(){
+        self.visible=0;
+        if(self.operation=="Modifier")
+            self.update();
+        else{
+            self.contacts.push(angular.copy(self.tmpcontact));
+        }
 
     };
 
     this.update=function(){
-
+        self.contact.firstname=angular.copy(self.tmpcontact.firstname);
+        self.contact.lastname=angular.copy(self.tmpcontact.lastname);
+        self.contact.mail=angular.copy(self.tmpcontact.mail);
     };
 
     this.delete=function(contact){
