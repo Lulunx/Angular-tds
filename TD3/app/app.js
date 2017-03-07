ad998990edee6727ebe24b35a304de16a31e3c13
Lulunx <lucasgancel@gmail.com>
2017-03-07 16:29:23 +0100

    Début broserify et avancée du convertisseur ( qui bug pour l'instant )

--- TD3/app/app.js
+++ TD3/app/app.js
@@ -10,7 +10,7 @@ CurrencyApp.controller("CurrencyController",['$http', function($http){
     this.what=1;
     this.result;
     this.histo=false;
-    this.listeconversions={};
+    this.listeconversion=[];
     $http.get('data/currencymap.json').
     then(function(response) {
             self.currencies = response.data;
@@ -25,15 +25,43 @@ CurrencyApp.controller("CurrencyController",['$http', function($http){
         this.inter=self.from;
         self.from=self.to;
         self.to=this.inter;
-    }
+    };
+
+    this.deleteHisto=function(histo){
+        self.listeconversion.splice(self.listeconversion.indexOf(histo),1);
+    };
+
+    this.updateHisto=function(histo){
+        $http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+self.from.code+'_'+self.to.code, {jsonpCallbackParam: 'callback'})
+            .then(function(response) {
+                var tx=response.data[histo.from.code+'_'+histo.to.code].val;
+                histo.date=Date.now();
+                histo.rate=tx;
+                histo.amount=histo.what*histo.rate;
+                histo.delta=histo.initialRate/histo.rate;
+            });
+    };
 
     this.getResult=function(){
         $http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+self.from.code+'_'+self.to.code, {jsonpCallbackParam: 'callback'})
             .then(function(response) {
                 var tx=response.data[self.from.code+'_'+self.to.code].val;
                 self.result=tx*self.what;
-                var conv={from:self.from,to:self.to,amount:self.result,initialAmount:self.result,delta:1,rate:tx,what:self.what,date:Date.now(),update:false,initialRate:tx};
-                self.listeconversion.push(conv);
+                var y=false;
+                for(var i=0;i<self.listeconversion.length;i++){
+                    if(self.listeconversion[self.to+"_"+self.from]!=null){
+                        y=true;
+                        self.listeconversion.indexOf(i).initialAmout=self.what;
+                        self.listeconversion.indexOf(i).amount=self.what;
+                        self.listeconversion.indexOf(i).date=Date.now();
+                        self.listeconversion.indexOf(i).rate=tx;
+                        self.listeconversion.indexOf(i).initialRate=tx;
+                    }
+                }
+                if(y==false){
+                    var conv={from:self.from,to:self.to,amount:self.result,initialAmount:self.result,delta:1,rate:tx,what:self.what,date:Date.now(),update:false,initialRate:tx};
+                    self.listeconversion[self.to+"_"+self.from]=conv;
+                }
             });
     }
 }]);
